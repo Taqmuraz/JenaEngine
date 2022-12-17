7de2c3c181b6804c3d4bf9b650a26d87280677c9
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2022-12-17 21:22:17 +0100

    build script for windows

--- build_windows.bat
+++ build_windows.bat
@@ -1,5 +1,9 @@
+rd /s /q build
 dir /s /b *.java > sourcelist.txt
 javac -d build @sourcelist.txt
 del sourcelist.txt
 
-rd /s /q build
+cd build
+(echo Main-Class: jena.swing.Program & echo. ) > manifest.txt
+jar -cvfm jena.jar manifest.txt jena
+java -jar jena.jar
