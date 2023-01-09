1eee89483bca11fec9ca06996188651d2f456263
taimuraz.fardzinov <taimuraz.fardzinov@aspose.com>
2023-01-09 18:29:10 +0100

    one hundred cycles

--- jena/swing/MainPanel.java
+++ jena/swing/MainPanel.java
@@ -11,10 +11,9 @@ import javax.swing.JPanel;
 import jena.engine.common.ErrorHandler;
 import jena.engine.entity.Camera;
 import jena.engine.entity.Player;
-import jena.engine.entity.Time;
-import jena.engine.graphics.ColorByteStruct;
 import jena.engine.graphics.GraphicsResource;
 import jena.engine.graphics.GraphicsDevice;
+import jena.engine.graphics.GraphicsDevicePainter;
 import jena.engine.graphics.TextureHandle;
 import jena.engine.io.FileResource;
 
@@ -22,7 +21,7 @@ public class MainPanel extends JPanel implements GraphicsResource
 {
 	private ErrorHandler errorHandler;
 	private Player player;
-	private Camera[] cameras;
+	private GraphicsDevicePainter rootPainter;
 	private BufferedImage screenBuffer;
 	private int canvasWidth;
 	private int canvasHeight;
@@ -37,12 +36,21 @@ public class MainPanel extends JPanel implements GraphicsResource
 		
 		player = new Player(this, keyboard);
 
-		cameras = new Camera[] 
+		var cameras = java.util.stream.IntStream.range(0, 100).boxed().map(i ->
 		{
-			new Camera(acceptor -> acceptor.call(0, 0, canvasWidth, canvasHeight), new ColorByteStruct(0, 50, 50, 255), player),
-			new Camera(acceptor -> acceptor.call(canvasWidth * (0.1f + (float)Math.cos(Time.time()) * 0.1f), canvasHeight * 0.25f, canvasWidth * 0.2f, canvasHeight * 0.4f), new ColorByteStruct(50, 50, 150, 255), player),
-			//new Camera(acceptor -> acceptor.call(canvasWidth * 0.4f, canvasHeight * (0.6f + (float)Math.sin(Time.time()) * 0.1f), canvasWidth * (0.3f + (float)Math.cos(Time.time()) * 0.2f), canvasHeight * 0.3f), new ColorByteStruct(50, 150, 150, 255), player),
+			float x = (i % 10) * 0.1f;
+			float y = (i / 10) * 0.1f;
+			return new Camera(a -> a.call(x * canvasWidth, y * canvasHeight, canvasWidth * 0.1f, canvasHeight * 0.1f), new jena.engine.graphics.ColorFloatStruct(x * 0.5f, y * 0.5f, 0f, 1f), player);
+		}).toList();
+
+		rootPainter = device ->
+		{
+			for(Camera camera : cameras)
+			{
+				camera.paint(device);
+			}
 		};
+
 		screenBuffer = new BufferedImage(canvasWidth, canvasHeight, BufferedImage.TYPE_INT_ARGB);
 	}
 
@@ -56,7 +64,7 @@ public class MainPanel extends JPanel implements GraphicsResource
 	{
 		Graphics2D screen = screenBuffer.createGraphics();
 		GraphicsDevice device = new SwingGraphicsDevice(screen);
-		for (Camera camera : cameras) camera.paint(device);
+		rootPainter.paint(device);
 
 		int w = getWidth();
 		int h = getHeight();
