94dfc877c38d3e3f99d399efe182609514587e1e
Taqmuraz <lulookgamedev@gmail.com>
2023-01-23 20:58:50 +0100

    linux build script update

--- build_linux.sh
+++ build_linux.sh
@@ -4,7 +4,8 @@ mkdir build
 javac -classpath "lib/*" -d build @sourcelist.txt
 rm sourcelist.txt
 cd build
-(echo Class-Path: ../lib/jogl-all.jar ../lib/gluegen-rt.jar & echo & echo Main-Class: jena.main.Program) > manifest.txt
+echo Main-Class: jena.main.Program >> manifest.txt
+echo Class-Path: ../lib/jogl-all.jar ../lib/gluegen-rt.jar >> manifest.txt
 jar -cvfm jena.jar manifest.txt jena
 cd ..
 sh run_linux.sh
